a94013d0094713be70c91adb1299d58e675726cb
man <man@phuektsofware.net>
2018-05-26 16:50:44 +0700

    BootStrap

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
 
+const user = [
+  {
+    Articles: "Node Js ServerSide",
+    Author: "Mr.xxx"
+  },
+
+  {
+    Articles: "Express Js ServerSide",
+    Author: "Mr.yyyy"
+  },
+
+  {
+    Articles: "React Coding",
+    Author: "Mr.CCC"
+  },
+
+  {
+    Articles: "Vuejs Documents",
+    Author: "Mr.YYY"
+  }
+];
+
 app.set("view engine", "ejs");
 
-app.get("/*", (req, res) => {
-  res.render("view");
+app.get("/", (req, res) => {
+  res.render("view", { data: user });
 });
 
 app.listen(3000, () => {
